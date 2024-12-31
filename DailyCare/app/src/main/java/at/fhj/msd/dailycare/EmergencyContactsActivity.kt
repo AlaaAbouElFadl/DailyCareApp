43d51ba957ab7cbff5c403df901e32dd9f86516e
AlaaAbouElFadl <alaa.abouelfadl@edu.fh-joanneum.at>
2024-12-31 01:31:42 +0100

    Add EmergencyContacts and Settings Activity

---
+++ DailyCare/app/src/main/java/at/fhj/msd/dailycare/EmergencyContactsActivity.kt
@@ -0,0 +1,20 @@
+package at.fhj.msd.dailycare
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class EmergencyContactsActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_emergency_contacts)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+    }
+}
